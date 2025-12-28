82b403a5c52a023d3e70c77158efdcc92305bda7
Avram Score <ascore@gmail.com>
2025-12-28 04:02:58 +0100

    Initial Commit

---
+++ Project Elysium/Project_ElysiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_ElysiumApp.swift
+//  Project Elysium
+//
+//  Created by Avram Score on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Project_ElysiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
